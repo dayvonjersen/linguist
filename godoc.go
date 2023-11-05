fc1e8a68585c6904c5350e77835074e524ee3dae
Ian Lewis <ianmlewis@gmail.com>
2023-11-05 02:06:55 +0000

    docs: Update package godoc
    Updates the package doc comment to add a note about parallel execution
    since methods are not safe to be called from multiple goroutines.
    
    Package doc comment was also formatted using Godoc best practices.
    https://tip.golang.org/doc/comment
    
    Fixes #12
    
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

--- godoc.go
+++ godoc.go
@@ -1,29 +1,27 @@
-/*
-Detect programming language of source files.
-Go port of GitHub Linguist: https://github.com/github/linguist
-
-Prerequisites:
-
-    go get github.com/jteeuwen/go-bindata/go-bindata
-
-Installation:
-
-    mkdir -p $GOPATH/src/github.com/dayvonjersen/linguist
-    git clone --depth=1 https://github.com/dayvonjersen/linguist $GOPATH/src/github.com/dayvonjersen/linguist
-    go get -d github.com/dayvonjersen/linguist
-    cd $GOPATH/src/github.com/dayvonjersen/linguist
-    make
-    l
-
-Usage:
-
-Please refer to the source code for the reference implementation at:
-
-https://github.com/dayvonjersen/linguist/tree/master/cmd/l
-
-
-See also:
-
-https://github.com/dayvonjersen/linguist/tree/master/tokenizer
-*/
+// Package linguist Detect programming language of source files.
+// Go port of GitHub Linguist: https://github.com/github/linguist
+//
+// Unless otherwise noted, callers should not assume APIs in this package are
+// safe for parallel execution.
+//
+// Prerequisites:
+//
+//	go get github.com/jteeuwen/go-bindata/go-bindata
+//
+// Installation:
+//
+//	mkdir -p $GOPATH/src/github.com/dayvonjersen/linguist
+//	git clone --depth=1 https://github.com/dayvonjersen/linguist $GOPATH/src/github.com/dayvonjersen/linguist
+//	go get -d github.com/dayvonjersen/linguist
+//	cd $GOPATH/src/github.com/dayvonjersen/linguist
+//	make
+//	l
+//
+// Usage:
+//
+//	Please refer to the source code for the reference implementation at:
+//	https://github.com/dayvonjersen/linguist/tree/master/cmd/l
+//
+//	See also:
+//	https://github.com/dayvonjersen/linguist/tree/master/tokenizer
 package linguist
